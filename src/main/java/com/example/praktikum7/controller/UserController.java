5a0a901cc9bb1fcb696a45e7f7f0778cc39178c3
asyiraafnufail <asyiraaf4@gmail.com>
2026-04-20 10:44:58 +0700

    menambahkan fungsi update

--- src/main/java/com/example/praktikum7/controller/UserController.java
+++ src/main/java/com/example/praktikum7/controller/UserController.java
@@ -1,4 +1,47 @@
 package com.example.praktikum7.controller;
 
+import com.example.praktikum7.model.User;
+import com.example.praktikum7.service.UserService;
+import org.springframework.boot.webmvc.autoconfigure.WebMvcProperties;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public String createUser(@RequestBody User request) {
+        userService.addUser(request);
+        return "user created successfully";
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable String id) {
+        return userService.getUserById(id);
+    }
+
+    @DeleteMapping("/{id")
+    public String deleteUser(@PathVariable String id){
+        userService.deleteUser(id);
+        return "user deleted successfully";
+    }
+
+    @PutMapping("/{id}")
+    public String updateUser(@PathVariable String id, @RequestBody User request){
+        userService.updateUser(id, request);
+        return "user updated";
+    }
 }
