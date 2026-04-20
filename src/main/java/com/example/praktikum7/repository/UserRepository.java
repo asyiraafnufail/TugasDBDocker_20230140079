1ec0e9194501598e5c5fc00e0dca37bbaa967468
asyiraafnufail <asyiraaf4@gmail.com>
2026-04-20 10:03:13 +0700

    mengisi file repository untuk interface

--- src/main/java/com/example/praktikum7/repository/UserRepository.java
+++ src/main/java/com/example/praktikum7/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.example.praktikum7.repository;
 
-public interface UserRepository {
+import com.example.praktikum7.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
 }
